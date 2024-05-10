baf38958804db5351fa9ce459be084c13ad98cbc
Alejandro Armas <lxarmas@gmail.com>
2024-05-10 12:48:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Footer from './components/Footer';
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <>
+          <div className='mainNavbar'>
+              <img className='imageNavbar' src="./public/3.png" ></img>
+             
+       </div>
+      </>
+    
+  )
+}
+
+export default Navbar
