3fba0b2abcd8d2db2dd2e2a4befe0f95490a02ca
Alejandro Armas <lxarmas@gmail.com>
2024-05-11 22:04:51 +0200

    props_added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Card from './components/Card';
+import data from './components/data';
 function App() {
 
-
+  const cardElements = data.map( card => {
+    return <Card
+      key={card.id}
+      img={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      location={card.location}
+      title={card.title}
+      price={card.price}
+    />
+    
+    
+})
   return (
-    <>
-      <Navbar />
-      <Hero />
   
-      <Footer/>
-    </>
+     <div>
+      <Navbar />
+      <Hero/>
+            <section className="cards-list">
+                {cardElements}
+            </section>
+        </div>
+   
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from "react"
 
-const Card = (props) => {
-  return (
-    <>
-    
-      <div className='card'>
-          <img src={`./public/images/${props.img}`} className='card--image'/> 
-          <div className='card--stats'>
-              <img src='./public/images/star.png' className='card--star'></img>
-              <span>{props.rating}</span>
-              <span className='gray'>({props.reviewCount}) *</span>
-              <span className='gray'>{props.location}</span>
-          </div>
-          <p>{props.title}</p>
-          
-          <p><span className='bold'>from {props.price}</span>/ person</p>
-    </div>
-  </>
-  )
+export default function Card(props) {
+    return (
+        <div className="card">
+            <img src={`../images/${props.img}`} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
 }
-
-export default Card
